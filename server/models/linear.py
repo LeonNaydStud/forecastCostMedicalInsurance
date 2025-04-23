73ab6d2c78f30ef019426b49f57ab081e17691fb
Leonid Naydanov <150089848+LeonNaydStud@users.noreply.github.com>
2025-04-24 00:41:57 +0700

    ...

--- server/models/linear.py
+++ server/models/linear.py
@@ -3,13 +3,13 @@ from sklearn import linear_model
 import json
 
 with open('D:/PyCharmProjects/forecastCostMedicalInsurance/models/linear/model.pickle', mode='rb') as filestream:
-    linear_model = pickle.load(filestream)
+    model = pickle.load(filestream)
 
 with open('D:/PyCharmProjects/forecastCostMedicalInsurance/models/linear/info.json', 'r', encoding='utf-8') as filestream:
     info = json.load(filestream)
 
 def calculate(age: float) -> float:
-    ans = linear_model.predict([[age]])[0][0]
+    ans = model.predict([[age]])[0][0]
     return ans
 
 def get_r2() -> float:
