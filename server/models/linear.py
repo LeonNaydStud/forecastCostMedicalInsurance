e2ef4ce41cfadc0764d41e619882043b242158b8
Leonid Naydanov <150089848+LeonNaydStud@users.noreply.github.com>
2025-04-24 00:45:22 +0700

    ...

--- server/models/linear.py
+++ server/models/linear.py
@@ -1,18 +1,30 @@
 import pickle
 import json
+import os
 
-with open('D:/PyCharmProjects/forecastCostMedicalInsurance/models/linear/model.pickle', mode='rb') as filestream:
-    model = pickle.load(filestream)
+# Get the directory where this script is located
+current_dir = os.path.dirname(__file__)
 
-with open('D:/PyCharmProjects/forecastCostMedicalInsurance/models/linear/info.json', 'r', encoding='utf-8') as filestream:
-    info = json.load(filestream)
+# Construct paths relative to the script location
+model_path = os.path.join(current_dir, '..', '..', 'models', 'linear', 'model.pickle')
+info_path = os.path.join(current_dir, '..', '..', 'models', 'linear', 'info.json')
+
+# Load the model and info
+with open(model_path, 'rb') as f:
+    model = pickle.load(f)
+
+with open(info_path, 'r', encoding='utf-8') as f:
+    info = json.load(f)
 
 def calculate(age: float) -> float:
+    """Predict insurance cost based on age"""
     ans = model.predict([[age]])[0][0]
     return ans
 
 def get_r2() -> float:
+    """Get R-squared score"""
     return info['r2']
 
 def get_rmse() -> float:
+    """Get RMSE score"""
     return info['rmse']
